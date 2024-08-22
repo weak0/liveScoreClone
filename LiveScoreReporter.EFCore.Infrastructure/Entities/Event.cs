5e46f10f31a4883a1466d815cf2d6e5a26895f5f
Piotr Szymala <p.szymala@sellpander.pl>
2024-08-22 16:25:29 +0200

    Provide event type enum.

--- LiveScoreReporter.EFCore.Infrastructure/Entities/Event.cs
+++ LiveScoreReporter.EFCore.Infrastructure/Entities/Event.cs
@@ -6,11 +6,19 @@
        
         public int GameId { get; set; }
         public int TeamId { get; set; }
-        public string Type { get; set; }
+        public EventType Type { get; set; }
         public string Details{ get; set; }
         public string Comments { get; set; }
         public int Time { get; set; }
         public int PlayerId { get; set; }
         public int? AssistPlayerId { get; set; }
+    }
+
+    public enum EventType
+    {
+        Goal,
+        Card,
+        Subst,
+        Var
     }
 }
