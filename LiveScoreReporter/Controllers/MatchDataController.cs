81a1c82bccf71aabba038fff494e2030de848876
Piotr Szymala <p.szymala@sellpander.pl>
2024-08-23 20:24:45 +0200

    Clean MatchDataCotroller (remove mock endpoint).

--- LiveScoreReporter/Controllers/MatchDataController.cs
+++ LiveScoreReporter/Controllers/MatchDataController.cs
@@ -1,4 +1,5 @@
 ﻿using LiveScoreReporter.Application.Services;
+using LiveScoreReporter.MockApiAssets.Services;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -8,8 +9,8 @@ namespace LiveScoreReporter.Controllers
     [ApiController]
     public class MatchDataController : ControllerBase
     {
-       private readonly IMatchService _matchService;
-
+        private readonly IMatchService _matchService;
+        
         public MatchDataController(IMatchService matchService)
         {
             _matchService = matchService;
@@ -18,9 +19,14 @@ namespace LiveScoreReporter.Controllers
         [HttpGet]
         public async Task<IActionResult> GetFixture(int fixtureId)
         {
-           var data = await _matchService.GetMatchDetailsAsync(fixtureId);
+            var data = await _matchService.GetMatchDetailsAsync(fixtureId);
+           
+            var result = await _matchService.AddDataToDb(data);
 
-           return Ok(data);
+            if (result)
+                return Ok(data);
+
+            return BadRequest();
         }
     }
 }
