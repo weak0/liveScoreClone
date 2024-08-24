1fefbd139a4b924189feefc62e9cf3d328417c5a
Piotr Szymala <p.szymala@sellpander.pl>
2024-08-24 20:17:28 +0200

    Move rabbit settings.

---
+++ LiveScoreReporter.Shared/RabbitMq/Settings/RabbitMqSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace LiveScoreReporter.Shared.RabbitMq.Settings
+{
+    public class RabbitMqSettings
+    {
+        public string Uri { get; set; }
+        public string ExchangeName { get; set; }
+        public string RoutingKey { get; set; }
+        public string QueueName { get; set; }
+    }
+}
