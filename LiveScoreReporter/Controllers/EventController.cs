6d0dbaa35eb67e81302bb2795aecc074cfe09146
Piotr Szymala <p.szymala@sellpander.pl>
2024-08-26 18:46:46 +0200

    Separate frontend controller into two controllers (event and game).

---
+++ LiveScoreReporter/Controllers/EventController.cs
@@ -0,0 +1,30 @@
+﻿using LiveScoreReporter.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveScoreReporter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        [HttpGet]
+        [Route("/events/{gameId}")]
+        public async Task<IActionResult> GetAllEventsForParticularGameAsync([FromRoute] int gameId)
+        {
+            var eventsWithPlayersAndTeams = await _eventService.GetGameEventsWithDetailsAsync(gameId);
+
+            var eventsWithDetailsDtos = _eventService.MapEventsToDto(eventsWithPlayersAndTeams);
+
+            var dtosSerializedToJson = _eventService.SerializeEventsToJson(eventsWithDetailsDtos);
+
+            return Ok(dtosSerializedToJson);
+        }
+    }
+}
