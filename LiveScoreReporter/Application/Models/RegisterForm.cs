6d11e6d495fb5ec72c9a3824df17a4fdc40099bb
Piotr Szymala <p.szymala@sellpander.pl>
2024-10-30 22:08:15 +0100

    Add login and register form models.

---
+++ LiveScoreReporter/Application/Models/RegisterForm.cs
@@ -0,0 +1,8 @@
+﻿namespace LiveScoreReporter.Application.Models
+{
+    public record RegisterForm
+    {
+        public string Login { get; init; } = string.Empty;
+        public string Passwd { get; init; } = string.Empty;
+    }
+}
