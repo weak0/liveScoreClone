e5e3a1baa459c9083cec068747cda34d2c6059e4
Piotr Szymala <p.szymala@sellpander.pl>
2024-10-30 22:08:44 +0100

    Create and register user Entity.

---
+++ LiveScoreReporter.EFCore.Infrastructure/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveScoreReporter.EFCore.Infrastructure.Entities
+{
+    public class User 
+    {
+        [Key]
+        public int Id { get; init; }
+
+        public DateTime CreatedAtUtc { get; init; } = DateTime.UtcNow;
+        public string Username { get; init; }
+        public string PasswordHash { get; init; }
+    }
+}
