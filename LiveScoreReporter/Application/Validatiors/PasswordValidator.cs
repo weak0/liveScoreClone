191b2c68a5e8778a8a77009349644e9f088db987
Piotr Szymala <p.szymala@sellpander.pl>
2024-10-30 22:08:00 +0100

    Add Validators for passwords and usernames.

---
+++ LiveScoreReporter/Application/Validatiors/PasswordValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using LiveScoreReporter.Application.Constants;
+
+namespace LiveScoreReporter.Application.Validatiors
+{
+    public class PasswordValidator : AbstractValidator<string>
+    {
+        public PasswordValidator()
+        {
+            RuleFor(password => password)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("Password cannot be empty")
+                .MinimumLength(LoginAndPasswordSettings.MinPasswordLength)
+                .WithMessage($"Password must be at least {LoginAndPasswordSettings.MinPasswordLength} characters long")
+                .MaximumLength(LoginAndPasswordSettings.MaxPasswordLength)
+                .WithMessage($"Password cannot be longer than {LoginAndPasswordSettings.MaxPasswordLength} characters");
+        }
+    }
+}
