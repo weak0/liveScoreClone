f2f20c2a3ef728485e0edc7216b88a28dbf81057
Piotr Szymala <p.szymala@sellpander.pl>
2024-10-30 22:07:23 +0100

    Crate commands and handlers for registering.

---
+++ LiveScoreReporter/Application/Commands/RegisterCommand.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using LiveScoreReporter.Application.Models;
+using LiveScoreReporter.Application.Validatiors;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace LiveScoreReporter.Application.Commands
+{
+    public record RegisterCommand : IRequest<bool>
+    {
+        public RegisterCommand(RegisterForm input)
+        {
+            Username = input.Login;
+            Password = input.Passwd;
+        }
+
+        public string Username { get; init; }
+        public string Password { get; init; }
+    }
+
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(command => command.Username)
+                .SetValidator(new UsernameValidator());
+
+            RuleFor(command => command.Password)
+                .SetValidator(new PasswordValidator());
+        }
+    }
+}
---
+++ LiveScoreReporter/Application/Handlers/RegisterCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using LiveScoreReporter.Application.Commands;
+using LiveScoreReporter.Application.Services.Interfaces;
+using MediatR;
+
+namespace LiveScoreReporter.Application.Handlers
+{
+    public class RegisterCommandHandler : IRequestHandler<RegisterCommand, bool>
+    {
+        private readonly IAuthService _authService;
+
+        public RegisterCommandHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<bool> Handle(RegisterCommand command, CancellationToken cancellationToken)
+        {
+            var result = await _authService.Register(command.Username, command.Password);
+
+            return result;
+        }
+    }
+}
