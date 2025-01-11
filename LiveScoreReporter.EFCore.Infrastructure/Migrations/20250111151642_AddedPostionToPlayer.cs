5131f0d9580842a57a73ed7fe7a1da2e8e965b0c
weak0 <maciej.gorzela89@gmail.com>
2025-01-11 16:44:50 +0100

    Added player entity migration

---
+++ LiveScoreReporter.EFCore.Infrastructure/Migrations/20250111151642_AddedPostionToPlayer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LiveScoreReporter.EFCore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedPostionToPlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Postition",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Postition",
+                table: "Players");
+        }
+    }
+}
